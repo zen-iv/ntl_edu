4eb973e63e84a0c5e571f421e3b6efed26436c29
aizen <nekomata.j@gmail.com>
2023-01-29 10:36:48 +0300

    hw 2.1.1 added

---
+++ netology_hw2.1.1/netology_hw2.1.1.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <fstream>
+
+int main()
+{
+    std::ifstream intxt("in.txt");
+    int N, M, x;
+    if (intxt.is_open()) {
+
+        intxt >> N;
+        int* arr1 = new int[N];
+        for (int i = 0; i < N; i++)
+        {
+            intxt >> arr1[i];
+        }
+
+        intxt >> M;
+        int* arr2 = new int[M];
+        for (int i = 0; i < M; i++)
+        {
+            intxt >> arr2[i];
+        }
+
+        std::ofstream outtxt("out.txt");
+
+        outtxt << M << std::endl;
+        outtxt << arr2[M - 1] << " ";
+        for (int i = 0; i < M - 1; i++) {
+            outtxt << arr2[i] << " ";
+        }
+
+        outtxt << std::endl;
+        outtxt << N << std::endl;
+        for (int i = 1; i < N; i++) {
+            outtxt << arr1[i] << " ";
+        }
+        outtxt << arr1[0];
+
+        outtxt.close();
+
+        delete[] arr1;
+        arr1 = nullptr;
+        delete[] arr2;
+        arr2 = nullptr;
+
+        intxt.close();
+    }
+    else {
+        std::cout << "Не получилось открыть файл!" << std::endl;
+    }
+}
