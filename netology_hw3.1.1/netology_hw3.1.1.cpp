cbe09b048794156ec2a0a299bfa9ba7e285ff543
Maxim P <nekomata.j@gmail.com>
2023-05-03 23:50:24 +0500

    Added netology_hw3.1.X

---
+++ netology_hw3.1.1/netology_hw3.1.1.cpp
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая все нечётные элементы массива (std::vector) умножает на 3 с помощью лямбда-функции.
+// Циклы использовать не нужно.
+/*  Входные данные:  4 7 9 14 12 
+    Выходные данные: 4 21 27 14 12 */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    std::vector<int> vec = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    std::for_each(vec.begin(), vec.end(), [](int& x) { //можно через for(auto & x : vec)...
+        if (x % 2 == 1) {
+            x *= 3;
+        }
+        });
+    for (int x : vec) {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
---
+++ netology_hw3.1.2/netology_hw3.1.2.cpp
@@ -0,0 +1,55 @@
+﻿/*
+std::variant бывает удобно применять в разработке парсеров.
+
+Например, есть некоторая универсальная функция, которая может возвращать значения разных типов в зависимости от контекста.В этом задании вам нужно использовать простую функцию, которая может возвращать число, строку или массив чисел.
+Вам нужно написать обработку результата этой функции следующим образом :
+
+Если в результате функции содержится int, то вывести число, умноженное на 2.
+Если в результате функции содержится строка, то просто вывести её в консоль.
+Если в результате функции содержится вектор чисел, то вывести все его элементы в консоль.
+
+Поменять стандарт C++
+*/
+#include <iostream>
+#include <variant>
+#include <vector>
+#include <string>
+
+std::variant<int, std::string, std::vector<int>> get_variant() {
+	std::srand(std::time(nullptr));
+	int random_variable = std::rand() % 3;
+
+	std::variant<int, std::string, std::vector<int>> result;
+	switch (random_variable)
+	{
+	case 0:
+		result = 5;
+		break;
+	case 1:
+		result = "string";
+		break;
+	case 2:
+		result = std::vector<int>{ 1, 2, 3, 4, 5 };
+		break;
+	default:
+		break;
+	}
+	return result;
+}
+
+int main() {
+	auto result = get_variant();
+	if (auto ptr = std::get_if<int>(&result)) { // get_if - получить указатель на объект определенного типа, и проверяем, что указатель не равен nullptr.
+		std::cout << *ptr * 2 << std::endl;
+	}
+	else if (auto ptr = std::get_if<std::string>(&result)) {
+		std::cout << *ptr << std::endl;
+	}
+	else if (auto ptr = std::get_if<std::vector<int>>(&result)) {
+		for (int x : *ptr) {
+			std::cout << x << " ";
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
