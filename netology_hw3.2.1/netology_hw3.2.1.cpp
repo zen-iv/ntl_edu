84e745550f46b1649eb5edf75eb5b60a4e0c0fde
Maxim P <nekomata.j@gmail.com>
2023-05-09 08:45:41 +0500

    Added netology_hw3.2.X

---
+++ netology_hw3.2.1/netology_hw3.2.1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+// убран using namespace std;
+struct point {
+	double m_x;
+	double m_y;
+	point(double x, double y) {
+		m_x = x;
+		m_y = y; // y = y заменено
+	}
+};
+
+void print_point(const point& point_object) {
+	std::cout << "x:" << point_object.m_x << ", y: " << point_object.m_y << std::endl;
+};
+
+int main() // изменено название с Main -> main
+{
+	for (int i = 0; i < 5; i++) { // определение переменной i перенесенов в тело цикла, лишняя точка с запятой
+		point my_point(i, 2 * i);
+		print_point(my_point);
+	}
+	return 0;
+}
+//убранна копия void print_point
