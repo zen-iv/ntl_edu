a97f944fe7c65e53372be32469690208f0a9fbc5
zen-iv <nekomata.jp@yandex.ru>
2023-06-21 22:10:10 +0500

    Added "hw3.7.X"
    Added "hw3.6\7.X"

---
+++ netology_hw3.7.1/netology_hw3.7.1.cpp
@@ -0,0 +1,74 @@
+﻿/*
+Задача 1. Частота символов
+Описание
+Нужно посчитать, сколько раз каждый символ встречается в заданном тексте, и отсортировать по убыванию частоты.
+
+[IN]: Hello world!!
+[OUT]:
+l: 3
+
+*/
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <cctype> 
+#include <algorithm>
+
+using namespace std;
+class sort_string
+{
+public:
+	void show_list(string* UserInput) {
+		str = *UserInput;
+		/*for (char& c : str) {
+			c = tolower(c);
+		}*/
+		cout << "[IN]: " << str << endl;
+		make_letter_count_list();
+
+		cout << "[OUT]:\n";
+
+		make_sorted_list();
+		for (const auto& el : sorted_list)//вывод на экран
+		{
+			cout << el.second << ": " << el.first << endl;
+		}
+	}
+
+private:
+	string str;
+
+	map<int, char> unsorted_list;
+	multimap<int, char, greater<int>> sorted_list;
+
+	void make_letter_count_list() { //запись букв и колличества
+		int i = 0;
+		char letter;
+		int quantity;
+		while (letter = str[i])
+		{
+			quantity = count(str.begin(), str.end(), letter);
+			unsorted_list.emplace(make_pair(letter, quantity));
+			++i;
+		}
+	}
+
+	void make_sorted_list() {//multimap с key = quantity для сортировки по убыванию кол-ва
+		for (const auto& [letter, quantity] : unsorted_list)
+		{
+			sorted_list.emplace(make_pair(quantity, letter));
+		}
+	}
+};
+
+int main()
+
+{
+	setlocale(LC_ALL, "Russian");
+	string inputText;
+	cout << "Введите текст: ";
+	getline(cin, inputText);
+	sort_string Ex;
+	Ex.show_list(&inputText);
+}
---
+++ netology_hw3.7.2/netology_hw3.7.2.cpp
@@ -0,0 +1,37 @@
+﻿//Задача 2. Удаление дубликатов
+//Описание
+//Вам поступают числа из стандартного потока ввода.Первым числом вводится количество элементов, далее сами значения.
+//
+//Нужно вывести все эти числа без дубликатов в порядке убывания.
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+class SortNumbers {
+public:
+    void showSortedNumbers() {
+        int count;
+        std::cout << "[IN]:\n";
+        std::cin >> count;
+
+        std::set<int, std::greater<int>> numbersSet;
+        int num;
+        for (int i = 0; i < count; ++i) {
+            std::cin >> num;
+            numbersSet.insert(num);
+        }
+
+        std::cout << "[OUT]:\n";
+        for (int number : numbersSet) {
+            std::cout << number << std::endl;
+        }
+    }
+};
+
+int main() {
+    SortNumbers sortNumbers;
+    sortNumbers.showSortedNumbers();
+
+    return 0;
+}
